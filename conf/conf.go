1a3718fb79ada004aa099f5be7c9d8daec5b2b0d
Christopher Liu <chrisckeyliu@gmail.com>
2024-01-17 01:05:05 +0800

    [Upd] Add New Config

--- conf/conf.go
+++ conf/conf.go
@@ -2,6 +2,8 @@
 package conf
 
 import (
+	"enrollment/logger"
+	"errors"
 	"os"
 
 	"github.com/pelletier/go-toml/v2"
@@ -11,10 +13,11 @@ const Fn = "config.toml"
 
 // Account
 type Account struct {
-	No       string   `toml:"no"`       // Student Number
-	Pw       string   `toml:"password"` // Password
-	Comment  string   `toml:"comment"`  // comment
-	CourseNo []string `toml:"courses"`  // Course Numbers
+	No            string   `toml:"no"`             // Student Number
+	Pw            string   `toml:"password"`       // Password
+	Comment       string   `toml:"comment"`        // comment
+	CourseNo      []string `toml:"courses"`        // Course Numbers
+	CourseComment []string `toml:"coursesComment"` // courseNames or others
 }
 
 // Program Specification
@@ -52,6 +55,14 @@ func (c *Conf) LoadConfig() error {
 	return c.clean()
 }
 
+var ErrorConfig = errors.New("configuration error")
+
 func (c *Conf) clean() (err error) {
-	return nil
+	for _, a := range c.Ac {
+		if len(a.CourseNo) != len(a.CourseComment) {
+			err = ErrorConfig
+			logger.Errorf("%v Must Aligned with %v", a.CourseNo, a.CourseComment)
+		}
+	}
+	return err
 }
